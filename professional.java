102be0421cb06420181b0cee9556fbab3dab32c6
Kavitha Polam <kavitha.polam@ibm.com>
2021-02-12 14:52:06 +0530

    personel

---
+++ professional.java
@@ -0,0 +1,8 @@
+public class professional
+{
+     public Static void main(String args[])
+          {
+              System.out.println("company name=IBM");
+              System.out.println("role=Developer");
+            }   
+}
